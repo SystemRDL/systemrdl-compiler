807d7efe7de00950df675e78249dcada298b6cd1
Alex Mykyta <amykyta3@users.noreply.github.com>
2019-01-23 20:09:40 -0800

    Bring forward more contents into top namespace

--- systemrdl/__init__.py
+++ systemrdl/__init__.py
@@ -1,4 +1,11 @@
 from .__about__ import __version__
+
 from .compiler import RDLCompiler
 from .walker import RDLListener, RDLWalker
 from .messages import RDLCompileError
+
+from .node import AddressableNode, VectorNode, SignalNode
+from .node import FieldNode, RegNode, RegfileNode, AddrmapNode, MemNode
+
+from .component import AddressableComponent, VectorComponent, Signal
+from .component import Field, Reg, Regfile, Addrmap, Mem
