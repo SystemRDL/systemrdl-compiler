78d6fdbb63ca4b22faab3d1e80682fc936c0f422
Alex Mykyta <amykyta3@users.noreply.github.com>
2019-01-24 22:02:16 -0800

    more importer updates

--- systemrdl/importer.py
+++ systemrdl/importer.py
@@ -19,18 +19,29 @@ class RDLImporter:
             raise ValueError("Component must have a type_name")
 
         if definition.is_instance:
-            raise ValueError("Component cannot be instantiated")
+            raise ValueError("Component cannot already be instantiated")
 
+        if len(self.compiler.namespace.type_ns_stack) != 1:
+            raise RuntimeError("Namespace was not properly reset to root scope")
+
+        # Register with namespace
         self.compiler.namespace.register_type(
             definition.type_name,
             definition,
             definition.def_src_ref
         )
 
+        # Add to root component definition list
+        self.compiler.root.comp_defs[definition.type_name] = definition
+
     def lookup_root_component(self, type_name:str):
-        t = self.compiler.namespace.lookup_type(type_name)
+        return self.compiler.root.comp_defs.get(type_name, None)
 
-        if not isinstance(Component):
-            t = None
-
-        return t
+    def assign_property(self, component, prop_name, value, src_ref):
+        rule = self.compiler.env.property_rules.lookup_property(prop_name)
+        if rule is None:
+            self.msg.fatal(
+                "Unrecognized property '%s'" % prop_name,
+                src_ref
+            )
+        rule.assign_value(component, value, src_ref)
