7af80def9538066090040a3433b57c0694aca8cc
Alex Mykyta <amykyta3@users.noreply.github.com>
2019-05-06 21:10:02 -0700

    Version update

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setuptools.setup(
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
