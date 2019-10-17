7e9c8e8c918ddd4e1850dfecf6796300e4122f90
Alex Mykyta <amykyta3@users.noreply.github.com>
2019-10-16 21:51:52 -0700

    Add testcase to catch type cast bug reported in #35

---
+++ test/test_prop_typecast.py
@@ -0,0 +1,22 @@
+
+from .unittest_utils import RDLSourceTestCase
+import systemrdl.rdltypes as rdlt
+
+class TestIntr(RDLSourceTestCase):
+
+    def test_intr(self):
+        top = self.compile(
+            ["rdl_testcases/property_typecast.rdl"],
+            "enum_cast"
+        )
+
+        f1 = top.find_by_path("enum_cast.r1.f1")
+        f2 = top.find_by_path("enum_cast.r1.f2")
+        f3 = top.find_by_path("enum_cast.r1.f3")
+        f4 = top.find_by_path("enum_cast.r1.f4")
+
+        self.assertEqual(f1.get_property("reset"), 3)
+        self.assertEqual(f1.get_property("we"), True)
+        self.assertEqual(f2.get_property("we"), True)
+        self.assertEqual(f3.get_property("we"), False)
+        self.assertEqual(f4.get_property("we"), False)
