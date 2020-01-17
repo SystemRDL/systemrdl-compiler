e9b18b9f5424b7660971b48cfbd0ee8c4729446e
Alex Mykyta <amykyta3@users.noreply.github.com>
2020-01-16 20:54:44 -0800

    Upgrade to antlr 4.8

--- systemrdl/parser/generate_parser.sh
+++ systemrdl/parser/generate_parser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-antlr4="java -Xmx500M -cp /usr/local/lib/antlr-4.7.2-complete.jar org.antlr.v4.Tool"
+antlr4="java -Xmx500M -cp /usr/local/lib/antlr-4.8-complete.jar org.antlr.v4.Tool"
 antlr4py3="$antlr4 -Dlanguage=Python3"
 
 $antlr4py3 -visitor -no-listener SystemRDL.g4
