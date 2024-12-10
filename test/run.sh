5d3ad4e005de31c0758c9340b18eca80bb3003f5
Alex Mykyta <amykyta3@users.noreply.github.com>
2024-12-09 23:16:53 -0800

    Switch to ABI3 compatible wheels

--- test/run.sh
+++ test/run.sh
@@ -19,17 +19,12 @@ venv_bin=$this_dir/.venv/bin
 python3 -m venv $this_dir/.venv
 source $this_dir/.venv/bin/activate
 
-# Install test dependencies
-pip install -U pip setuptools wheel
-pip install pytest pytest-cov coverage pylint parameterized types-Markdown pytest-parallel
-# Exclude version due to: https://github.com/python/mypy/issues/16770
-pip install "mypy != 1.8.0"
-
-# Install dut
+# Install
 export SYSTEMRDL_REQUIRE_BINARY_BUILD=1
 cd $this_dir/../
-python $this_dir/../setup.py install
+python -m pip install .
 cd $this_dir
+python -m pip install -r requirements.txt pytest-parallel
 
 # Run unit tests while collecting coverage
 pytest --cov=systemrdl
@@ -48,4 +43,4 @@ mv $this_dir/out.json $this_dir/../examples/tiny.json
 pylint --rcfile $this_dir/pylint.rc -j 0 systemrdl
 
 # Run static type checking
-mypy $this_dir/../systemrdl
+mypy $this_dir/../src/systemrdl
